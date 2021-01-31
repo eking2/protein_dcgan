cbf808c0b34059a1ed3d14a6a1edff49c8c23529
eking2 <eking2@uci.edu>
2021-01-31 03:29:00 -0800

    finish models start lit

--- lit_dcgan.py
+++ lit_dcgan.py
@@ -1,5 +1,102 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
+from models import (Generator16, Generator64,
+                    Discriminator16, Discriminator64)
 import pytorch_lightning as pl
+import argparse
+import h5py
+
+
+class Distrograms(Dataset):
+    def __init__(self, path):
+        super().__init__()
+
+        self.maps = h5py.File(path, 'r')['arr']
+
+    def __len__(self):
+        return self.maps.shape[0]
+
+    def __getitem__(self, idx):
+        return torch.as_tensor(self.maps[idx, ...], dtype=torch.float)
+        
+
+class DCGAN(pl.LightningModule):
+    def __init__(self, batch_size, z_dim, learning_rate, beta1, beta2, **kwargs):
+        super().__init__()
+
+        self.save_hyperparameters()
+        self.fixed_z = torch.randn(8, self.hparams.z_dim, 1, 1)
+
+    def forward(self, z):
+        return self.gen(z)
+
+    def configure_optimizers(self):
+        pass
+
+    def generator_step(self, x):
+        pass
+
+    def discriminator_step(self, x):
+        pass
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        pass
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+
+        parser.add_argument('-lr', '--learning_rate', type=float, default=1e-4,
+                            help='learning rate (default: 1e-4)')
+        parser.add_argument('-b1', '--beta1', type=float, default=0.5,
+                            help='adam beta1 (default: 0.5)')
+        parser.add_argument('-b2', '--beta2', type=float, default=0.999,
+                            help='adam beta2 (default: 0.999)')
+
+        parser.add_argument('-z', '--z_dim', type=int, default=100,
+                            help='latent dim')
+        parser.add_argument('-d', '--downsample', type=int, default=10,
+                            help='downsampling constant (default: 10)')
+        parser.add_argument('-s', '--size', type=int, choices=[16, 64, 128], default=64,
+                            help='fragment length')
+
+        return parser
+
+
+def cli_main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--batch_size', type=int, default=64,
+                        help='batch size (default: 64)')
+    parser.add_argument('-p', '--path', type=str, required=True,
+                        help='dataset path fragments hdf5')
+    parser.add_argument('-e', '--epochs', type=int, default=50,
+                        help='epochs (default: 50)')
+
+    script_args, _ = parser.parse_known_args()
+
+    # parse args
+    parser = DCGAN.add_model_specific_args(parser)
+    args = parser.parse_args()
+
+    # data
+    dataset = Distrograms(script_args.path)
+    loader = DataLoader(dataset, batch_size = script_args.batch_size, shuffle=True, pin_memory=True)
+
+    # model
+
+
+    # train
+
+
+
+
+if __name__ == '__main__':
+
+    cli_main()
+    
+    #ds = Distrograms('./data/training_30_16.hdf5')
+    
--- models.py
+++ models.py
@@ -37,12 +37,15 @@ class EnforceSym(nn.Module):
 
 
 class Generator16(nn.Module):
-    def __init__(self):
+    def __init__(self, z_dim):
         super().__init__()
+
+        # H_out = (H_in - 1)*stride + H_filter - 2*padding
+        # 4 = (1-1)*1 + 4 - 2*0
 
         # input = (b, 100, 1, 1)
         self.net = nn.Sequential(
-                *gen_block(100, 512, 4, 1, 0),          # (b, 512, 4, 4)
+                *gen_block(z_dim, 512, 4, 1, 0),        # (b, 512, 4, 4)
                 *gen_block(512, 256, 3, 1, 1),          # (b, 256, 4, 4)
                 *gen_block(256, 128, 4, 2, 1),          # (b, 128, 8, 8)
                 *gen_block(128, 64, 4, 2, 1),           # (b, 64, 16, 16)
@@ -59,6 +62,9 @@ class Generator16(nn.Module):
 class Discriminator16(nn.Module):
     def __init__ (self):
         super().__init__()
+        
+        # H_out = (H_in - H_filter + 2*padding)/stride + 1 
+        # 16 = (16-3+ 2*1)/1 + 1
 
         # input = (b, 1, 16, 16)
         self.net = nn.Sequential(
@@ -76,16 +82,17 @@ class Discriminator16(nn.Module):
 
 
 class Generator64(nn.Module):
-    def __init__(self):
+    def __init__(self, z_dim):
         super().__init__()
 
+        # input = (b, 100, 1, 1)
         self.net = nn.Sequential(
-                *gen_block(100, 512, 4, 1, 0),           # (b, 512, 4, 4)
+                *gen_block(z_dim, 512, 4, 1, 0),         # (b, 512, 4, 4)
                 *gen_block(512, 256, 4, 2, 1),           # (b, 256, 8, 8)
                 *gen_block(256, 128, 4, 2, 1),           # (b, 128, 16, 16)
                 *gen_block(128, 64, 4, 2, 1),            # (b, 64, 32, 32)
                 nn.ConvTranspose2d(64, 1, 4, 2, 1),      # (b, 1, 64, 64)
-                nn.ReLU(),
+                nn.ReLU(inplace=True),
                 EnforceSym()
                 )
 
@@ -93,16 +100,18 @@ class Generator64(nn.Module):
 
         return self.net(x)
 
+
 class Discriminator64(nn.Module):
     def __init__(self):
         super().__init__()
 
+        # input = (b, 1, 64, 64)
         self.net = nn.Sequential(
-                *disc_block(1, 64, 4, 2, 1, bn=False),
-                *disc_block(64, 128, 4, 2, 1, bn=True),
-                *disc_block(128, 256, 4, 2, 1, bn=True),
-                *disc_block(256, 512, 4, 2, 1, bn=True),
-                nn.Conv2d(512, 1, 4, 1, 0),
+                *disc_block(1, 64, 4, 2, 1, bn=False),     # (b, 64, 32, 32)
+                *disc_block(64, 128, 4, 2, 1, bn=True),    # (b, 128, 16, 16)
+                *disc_block(128, 256, 4, 2, 1, bn=True),   # (b, 256, 8, 8)
+                *disc_block(256, 512, 4, 2, 1, bn=True),   # (b, 512, 4, 4)
+                nn.Conv2d(512, 1, 4, 1, 0),                # (b, 1, 1, 1)
                 nn.Sigmoid()
                 )
 
@@ -111,27 +120,75 @@ class Discriminator64(nn.Module):
         return self.net(x)
 
 
+class Generator128(nn.Module):
+    def __init__(self, z_dim):
+        super().__init__()
+
+        # input = (b, 100, 1, 1)
+        self.net = nn.Sequential(
+                *gen_block(z_dim, 512, 4, 4, 0),      # (b, 512, 4, 4)
+                *gen_block(512, 256, 4, 2, 1),        # (b, 256, 8, 8)
+                *gen_block(256, 128, 4, 4, 0),        # (b, 128, 32, 32)
+                *gen_block(128, 64, 4, 2, 1),         # (b, 64, 64, 64)
+                nn.ConvTranspose2d(64, 1, 4, 2, 1),   # (b, 1, 128, 128)
+                nn.ReLU(inplace=True),
+                EnforceSym()
+                )
+
+    def forward(self, x):
+
+        return self.net(x)
+
+
+class Discriminator128(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # input = (b, 1, 128, 128)
+        self.net = nn.Sequential(
+                *disc_block(1, 64, 4, 2, 1, bn=False),     # (b, 64, 64, 64)
+                *disc_block(64, 128, 4, 2, 1, bn=True),    # (b, 128, 32, 32)
+                *disc_block(128, 256, 4, 4, 0, bn=True),   # (b, 256, 8, 8)
+                *disc_block(256, 512, 4, 2, 1, bn=True),   # (b, 512, 4, 4)
+                nn.Conv2d(512, 1, 4, 1, 0),                # (b, 1, 1, 1)
+                nn.Sigmoid()
+                )
+
+    def forward(self, x):
+
+        return self.net(x)
+
+
+
 def test():
 
+    z_dim = 100
     gen_in = torch.randn(10, 100, 1, 1)
     print('gen input:', gen_in.shape)
     print()
 
     disc16_in = torch.randn(10, 1, 16, 16)
     disc64_in = torch.randn(10, 1, 64, 64)
+    disc128_in = torch.randn(10, 1, 128, 128)
 
-    model = Generator16()
+    model = Generator16(z_dim)
     print('gen16 output:', model(gen_in).shape)
     model = Discriminator16()
     print('disc16 output:', model(disc16_in).shape)
     print()
 
-    model = Generator64()
+    model = Generator64(z_dim)
     print('gen64 output:', model(gen_in).shape)
     model = Discriminator64()
     print('disc64 output:', model(disc64_in).shape)
     print()
 
+    model = Generator128(z_dim)
+    print('gen128 output:', model(gen_in).shape)
+    model = Discriminator128()
+    print('disc128 output:', model(disc128_in).shape)
+    print()
 
 
-test()
+if __name__ == '__main__':
+    test()
